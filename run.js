185f52efac35993bb465f579750a768dec689b05
Aksana <aksana@Aksanas-MacBook-Pro.local>
2016-07-18 20:09:47 +0300

    Fix issue with Assignment

--- run.js
+++ run.js
@@ -1,7 +1,10 @@
 var babel = require('babel-core');
 var script = require('./script');
 
-var code = 'console.log(process.env.whatever); var a = process.env.whatever';
+var code = `console.log(process.env.whatever); 
+            process.env.whatever=1; 
+            var a = process.env.whatever`;
+
 
 var out = babel.transform(code, {
     plugins: [script]
--- script.js
+++ script.js
@@ -4,14 +4,14 @@ module.exports = function(babel) {
   return {
     visitor: {
       MemberExpression(path) {
-          if (t.isIdentifier(path.node.property) && path.node.property.name === 'env'
-              && t.isIdentifier(path.node.object) && path.node.object.name === 'process'
-              && t.isIdentifier(path.parentPath.node.property)) {
-            var envValue = process.env[path.parent.property.name];
-            if (envValue) {
-              path.parentPath.replaceWith(t.stringLiteral(envValue));
-            }
-          }
+              if (t.isIdentifier(path.node.property) && path.node.property.name === 'env'
+                  && t.isIdentifier(path.node.object) && path.node.object.name === 'process'
+                  && t.isIdentifier(path.parentPath.node.property)) {
+                var envValue = process.env[path.parent.property.name];
+                if (envValue && !t.isAssignmentExpression(path.parentPath.parentPath.node)) {
+                   path.parentPath.replaceWith(t.stringLiteral(envValue));
+                }
+              }
       }
     }
   }
